40e4f50ed6af2185e24e4a19d196dbda0632e922
yakovmeister <so@tfwno.gf>
2019-01-04 17:05:45 +0800

    fix: import defaultErrorFunc

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import { defaultErrorFunc } from "@lambda/defaultErrorFunc"
 import { lambdaWrapper } from "@lambda/lambdaWrap"
 import { resolver, responser } from "@lambda/resolver"
 
 export {
+  defaultErrorFunc,
   lambdaWrapper,
   responser,
   resolver
